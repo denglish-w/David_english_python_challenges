2ec2f9e3504ae5e5526ef0f778dca857240c93bb
David English <dwenglish97@outlook.com>
2025-08-03 13:19:46 -0400

    final commit

---
+++ reverse_string.py
@@ -0,0 +1,14 @@
+# Your Task
+
+# Define a function called reverse_string that accepts one argument (a string).
+# Return the reversed version of the string.
+# Try using string slicing as a first approach.
+# Bonus: After you get it working, try writing a second version using a for loop to build the reversed string manually.
+
+
+def reverse_string(s):
+    print(s[::-1])
+
+reverse_string('Python')
+
+    
