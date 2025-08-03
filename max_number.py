a193063f2440888f61e22a675a7d9b2203863e33
David English <dwenglish97@outlook.com>
2025-08-03 15:32:39 -0400

    "first commit"

---
+++ max_number.py
@@ -0,0 +1,12 @@
+# Your Task
+
+# Define a function called find_max that accepts a list of integers or floats.
+# Return the largest number in the list.
+# Do not use the built-in max() function — try to solve it manually using:
+# A for loop to check each number
+# # A variable to track the current maximum
+
+
+def find_max(n: int | float) -> int | float:
+    for n in numbers:
+        
